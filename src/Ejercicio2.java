ddc1d31d109988deeb13c184d0da1dacb65932da
BRANDOL RIOS <sleyer13992@gmail.com>
2026-08-04 14:28:36 -0500

    Ejercicio2: (promedio del curso), solo queda sacar el promedio y poner un if para saber si paso o no

---
+++ src/Ejercicio2.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        int numEstudiantes = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos estudiantes desea ingresar?"));
+
+        double[] notas = new double[numEstudiantes];
+        int aprobados = 0;
+        int reprobados = 0;
+        double suma = 0;
+
+        for (int i = 0; i < numEstudiantes; i++) {
+
+            notas[i] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota del estudiante:"));
+
+            suma += notas[i];
+        }
+    }
+}
