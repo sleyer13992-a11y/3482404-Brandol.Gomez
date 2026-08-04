d026d43f92a4e6b830beb214820cbbcb094b80cc
BRANDOL RIOS <sleyer13992@gmail.com>
2026-08-04 13:58:35 -0500

    Ejercicio1: (pares e impares) Se logro corregir y completar el ejercicio

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,23 +1,26 @@
 import javax.swing.*;
 
 public class Ejercicio1 {
-     public  static void main(String[] args) {
-        int numIngresados = Integer.parseInt(JOptionPane.showInputDialog("Cuantos numero deseas ingresar?"));
-        int [] numeros = new int [numIngresados];
+    public static void main(String[] args) {
+
+        int numIngresados = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos números deseas ingresar?"));
+        int[] numeros = new int[numIngresados];
         int pares = 0;
         int impares = 0;
 
         for (int i = 0; i < numIngresados; i++) {
-            numeros = new int[]{Integer.parseInt(JOptionPane.showInputDialog("Ingrese los numeros"))};
+
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número"));
 
             if (numeros[i] % 2 == 0) {
-               numeros = new int[]{pares++};
-
-            } else  {
-                numeros= new int[]{impares++};
+                pares++;
+            } else {
+                impares++;
             }
         }
 
-        JOptionPane.showMessageDialog(null, "pares:" + pares + "\n" + " impares:" + impares);
-     }
+        JOptionPane.showMessageDialog(null,
+                "Pares: " + pares +
+                        "\nImpares: " + impares);
+    }
 }
