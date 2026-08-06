d8da40dc1afc113df12d027c9785f0660eeb8e5a
BRANDOL RIOS <sleyer13992@gmail.com>
2026-08-06 12:57:48 -0500

    Ejercicio4: (Clasificaion de temperaturas), EL codigo ya imprime el mensaje final correctamente con sus respectivas clasificaciones

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -4,14 +4,23 @@ public class Ejercicio4 {
     public static void main (String[] args){
 
         int NumeroTemperatura = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas temperaturas desea registrar?"));
-
         double[] temperaturas = new double[NumeroTemperatura];
+        String resultado = "Clasificación de temperaturas:\n";
 
         for (int i = 0; i < NumeroTemperatura; i++) {
             temperaturas[i] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la temperatura " + (i + 1) + " en °C:"));
         }
 
-        String resultado = "Clasificación de temperaturas:";
 
+        for (int i = 0; i < NumeroTemperatura; i++) {
+            if (temperaturas[i] < 10) {
+                resultado += temperaturas[i] + " °C -> Frío\n";
+            } else if (temperaturas[i] >= 10 && temperaturas[i] <= 25) {
+                resultado += temperaturas[i] + " °C -> Templado\n";
+            } else {
+                resultado += temperaturas[i] + " °C -> Calor\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null, resultado);
     }
 }
