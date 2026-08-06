b5e2d6dbc86849032499aa59bd4da300378a67c0
BRANDOL RIOS <sleyer13992@gmail.com>
2026-08-06 13:31:56 -0500

    Ejercicio5: (Busqueda de un numero), este ejercicio ta dificil toco pedir un poco de ayuda porque no me daba solo queda poner mensaje final

---
+++ src/Ejercicio5.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+
+        int CantidadNumeros = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de números que desea ingresar"));
+        int[] Numeros = new int[CantidadNumeros];
+
+        for (int i = 0; i < CantidadNumeros; i++) {
+            Numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese número " + (i + 1)));
+        }
+
+        int buscar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número que desea buscar"));
+
+        boolean existe = false;
+        String resultado = "";
+
+        for (int i = 0; i < CantidadNumeros; i++) {
+            if (Numeros[i] == buscar) {
+                existe = true;
+                resultado += "El número: " + buscar + " se encontró en la posición: " + i + "\n";
+            }
+        }
+    }
+}
